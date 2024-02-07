47602e3c8dee7c7e9eac4dfd93b52b0dcb92ba77
Igor Ruzhilov <iruzhilov@gmail.com>
2024-02-07 11:53:53 +0600

    init: Initial commit

---
+++ src/main/java/com/github/alwaysseen/merchsite/MerchsiteApplication.java
@@ -0,0 +1,13 @@
+package com.github.alwaysseen.merchsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerchsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerchsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/alwaysseen/merchsite/MerchsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.alwaysseen.merchsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerchsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
